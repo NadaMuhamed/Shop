8d57d7e5af2f149a69b00a2bf254710746ecde43
Nada Mohamed <nadamohamed1620019@gmail.com>
2026-04-15 17:34:14 +0200

    Implement product saving functionality and add products.json file

--- modles/product.js
+++ modles/product.js
@@ -1,4 +1,7 @@
 const products = [];
+const path = require('path');
+const rootDir = require('../utils/path');
+const fs = require('fs');
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +9,30 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(rootDir, 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err && fileContent.length > 0) {
+                try {
+                    products = JSON.parse(fileContent);
+                } catch (e) {
+                    products = [];
+                }
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll() {
-        return products;
+        const p = path.join(rootDir, 'data', 'products.json');
+        try {
+            const fileContent = fs.readFileSync(p);
+            return JSON.parse(fileContent);
+        } catch (err) {
+            return [];
+        }
     }
 }
