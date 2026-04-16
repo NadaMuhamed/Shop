9b02e5e6ecb8297aaac9e75345b82a03c98398ec
Nada Mohamed <nadamohamed1620019@gmail.com>
2026-04-16 21:25:13 +0200

    Refactor product fetching in shop controller to simplify code structure

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,22 +1,20 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
+  const products = Product.fetchAll();
+  res.render('shop/product-list', {
+    prods: products,
+    pageTitle: 'All Products',
+    path: '/products'
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
+  const products = Product.fetchAll();
+  res.render('shop/index', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/'
   });
 };
 
