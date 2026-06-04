239363fcc1d7539fc6aedc6f9ec976363f3cdc80
Nada Mohamed <nadamohamed1620019@gmail.com>
2026-06-04 11:10:24 +0300

    feat: establish Cart and CartItem models with associations to User and Product

--- models/cart-item.js
+++ models/cart-item.js
@@ -1,12 +1,15 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../utils/database');
-const Cart = sequelize.define('cart', {
+const CartItem = sequelize.define('cartItem', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true
-  }
+  },
+  quantity: Sequelize.INTEGER,
+  price: Sequelize.DOUBLE
 });
 
-module.exports = Cart;
+
+module.exports = CartItem;
