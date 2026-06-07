77479e933c4594bb22ff37184643b82bb3ea695e
Nada Mohamed <nadamohamed1620019@gmail.com>
2026-06-07 11:53:02 +0300

    feat: update cart retrieval to use Sequelize methods for improved data handling

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: false })
+  .sync({ alter: true })
   .then(result => {
     return User.findByPk(1);
   })
@@ -56,6 +56,8 @@ sequelize
     return user;
   })
   .then(user => {
+    return user.createCart()
+  }).then(cart => {
     app.listen(3000);
   })
   .catch(err => {
